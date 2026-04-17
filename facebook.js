0ae98960c4bf00bb992464c2de26c8a6242a05c6
ANDRI-STORE <andrizalhacieboan@gmail.com>
2026-04-17 12:20:02 +0700

    Add comments for source and author in facebook.js
    Added metadata comments for source and author information.

--- facebook.js
+++ facebook.js
@@ -1,3 +1,13 @@
+/*
+• Sumber : https://whatsapp.com/channel/0029VbBW0L5AYlUPmohzsS0Y
+*
+
+/*
+• Author : Andri Store
+
+• Nama fitur : Facebook downloader
+*/
+
 import axios from "axios"
 
 async function getToken() {
